86b9c67332e647e1324e63388c435378815a6520
Deams51 <mfourgeaud@gmail.com>
2017-09-27 20:24:12 +0200

    Added temp investor/creator menu to header

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  selected = 'CREATOR';
+  items = [
+    {text: 'Refresh'},
+    {text: 'Settings'},
+    {text: 'Help', disabled: true},
+    {text: 'Sign Out'}
+  ];
+
+  iconItems = [
+    {text: 'Redial', icon: 'dialpad'},
+    {text: 'Check voicemail', icon: 'voicemail', disabled: true},
+    {text: 'Disable alerts', icon: 'notifications_off'}
+  ];
+
+  select(text: string) { this.selected = text; }
 
   constructor() { }
 
