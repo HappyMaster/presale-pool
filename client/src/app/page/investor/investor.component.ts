5048037a2061ff7dbdd3775e6a0ffde967f33933
Deams51 <mfourgeaud@gmail.com>
2017-09-28 10:17:49 +0200

    Added bindings for Investor view

--- client/src/app/page/investor/investor.component.ts
+++ client/src/app/page/investor/investor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import * as accounting from 'accounting';
 
 @Component({
   selector: 'app-investor',
@@ -6,10 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./investor.component.css']
 })
 export class InvestorComponent implements OnInit {
+  contractAdress: string;
+  status: String;
+  maxAllocation: Number;
+  maxPerContributor: Number;
+  currentValue: Number;
+  minPerContributor: Number;
+  contributors: Array<Object>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {  }
 
   ngOnInit() {
+    this.contractAdress = this.route.snapshot.paramMap.get('address');
+    console.log('Investor => Current contract address: ', this.contractAdress);
+    this.dummyData();
   }
 
+  dummyData() {
+    this.status = 'PAID';
+    this.maxAllocation = 10000;
+    this.maxPerContributor = 900;
+    this.currentValue = 1000.01;
+    this.minPerContributor = 100;
+    this.contributors = [
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10},
+      {address: '0xD4D83f833f0185eB00788C4A5e85F1dfCD7C36D8', amount: 10}
+   ];
+  }
+
+  formatMoney(value) {
+    return accounting.formatMoney(value, { symbol: 'ETH',  format: '%v %s' });
+  }
+
+  getMyTokens() {
+    console.log('getMyTokens!');
+  }
+
+  withdrawEther() {
+    console.log('withdrawEther!');
+  }
 }
